883389838c5f8ab833f61692bfd5680d4580c7a3
voytus <voytus@protonmail.com>
2022-05-09 18:39:35 +0100

    WWTM added customers data

--- src/test/resources/sql-data.sql
+++ src/test/resources/sql-data.sql
@@ -1,1 +1,12 @@
 INSERT INTO `customers` (`first_name`, `surname`) VALUES ('jordan', 'harrison');
+INSERT INTO `customers` (`first_name`, `surname`) VALUES ('norman', 'goonies');
+INSERT INTO `customers` (`first_name`, `surname`) VALUES ('jennifer', 'ford');
+INSERT INTO `customers` (`first_name`, `surname`) VALUES ('anna', 'sierra');
+INSERT INTO `customers` (`first_name`, `surname`) VALUES ('john', 'bentley');
+INSERT INTO `customers` (`first_name`, `surname`) VALUES ('kate', 'dawidson');
+INSERT INTO `customers` (`first_name`, `surname`) VALUES ('melisa', 'tripalowski');
+INSERT INTO `customers` (`first_name`, `surname`) VALUES ('abraham', 'benzema');
+INSERT INTO `customers` (`first_name`, `surname`) VALUES ('ben', 'relie');
+
+
+
