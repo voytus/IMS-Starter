194f6d75084e81ac5eb8a321b3ae88a0c6374ce2
voytus <voytus@protonmail.com>
2022-05-09 15:50:07 +0100

    WTM added db properties, itemDAO, Itemcontroller partially

---
+++ src/main/java/com/qa/ims/controller/ItemController.java
@@ -0,0 +1,78 @@
+package com.qa.ims.controller;
+import java.util.List;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.qa.ims.persistence.dao.ItemDAO;
+import com.qa.ims.persistence.domain.Item;
+import com.qa.ims.utils.Utils;
+
+public class ItemController implements CrudController<Item> {
+
+	public static final Logger LOGGER = LogManager.getLogger();
+
+	private ItemDAO itemDAO;
+	private Utils utils;
+
+	public ItemController(ItemDAO itemDAO, Utils utils) {
+		super();
+		this.itemDAO = itemDAO;
+		this.utils = utils;
+	}
+
+	/**
+	 * Reads all items to the logger
+	 */
+	@Override
+	public List<Item> readAll() {
+		List<Item> items = itemDAO.readAll();
+		for (Item item  : items) {
+			LOGGER.info(item);
+		}
+		return items;
+	}
+
+	/**
+	 * Creates an item by taking in user input
+	 */
+	@Override
+	public Item create() {
+		LOGGER.info("Please enter a name of a first item");
+		String name = utils.getString();
+		LOGGER.info("Please enter an item price");
+		Integer  item_id = utils.getInteger();
+		Item item = itemDAO.create(new Item(item_id, item_name, item_price));
+		LOGGER.info("Item created");
+		return item;
+	}
+
+	/**
+	 * Updates an existing item by taking in user input
+	 */
+	@Override
+	public Item update() {
+		LOGGER.info("Please enter the id of the item you would like to update");
+		Long id = utils.getLong();
+		LOGGER.info("Please enter a first item");
+		String name = utils.getString();
+		LOGGER.info("Please enter an item name");
+		String item_price = utils.getString();
+		Item item = itemDAO.update(new Item(item_id, item_id, item_price));
+		LOGGER.info("Item updated");
+		return item;
+	}
+
+	/**
+	 * Deletes an existing item by the id of the customer
+	 * 
+	 * @return
+	 */
+	@Override
+	public int delete() {
+		LOGGER.info("Please enter the id of the item you would like to delete");
+		Long id = utils.getLong();
+		return itemDAO.delete(id);
+	}
+
+}
--- src/test/resources/sql-schema.sql
+++ src/test/resources/sql-schema.sql
@@ -4,33 +4,34 @@ CREATE SCHEMA IF NOT EXISTS `ims`;
 USE `ims`;
 
 CREATE TABLE IF NOT EXISTS `ims`.`customers` (
-    `customer_id` INT(11) NOT NULL AUTO_INCREMENT,
-    `first_name` VARCHAR(40) DEFAULT NULL,
-    `surname` VARCHAR(40) DEFAULT NULL,
-    PRIMARY KEY (`customer_id`)
+    customer_id INT(11) NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(40) DEFAULT NULL,
+    surname VARCHAR(40) DEFAULT NULL,
+    PRIMARY KEY (customer_id)
 );
 CREATE TABLE IF NOT EXISTS `ims`.`item` (
 	item_id int NOT NULL AUTO_INCREMENT,
 	item_price DEC(2,10) NOT NULL,
 	item_name VARCHAR(32),
-	PRIMARY KEY ('item_id),
+	PRIMARY KEY (item_id)
 );
 
 CREATE TABLE IF NOT EXISTS `ims`.`order_items` (
-	'order_id_item' int NOT NULL AUTO_INCREMENT,
-	'priceEach' DEC(2,10) NOT NULL,
-	'quantity' int NOT NULL,
-	PRIMARY KEY ('order_id_item')
-	FOREIGN KEY ('items') REFERENCES item('item_id')
+	order_item_id int NOT NULL AUTO_INCREMENT,
+    order_id int NOT NULL,
+    item_id int NOT NULL,
+	PRIMARY KEY (order_item_id),
+	FOREIGN KEY (item_id) REFERENCES item(item_id),
+	FOREIGN KEY (order_id) REFERENCES orders(order_id)
 );
 
 CREATE TABLE IF NOT EXISTS `ims`.`orders` (
-	'order_id' int NOT NULL AUTO_INCREMENT,
-	'order_id_item' INT,
-	'quantity' int NOT NULL);
-	PRIMARY KEY ('order_id),
-	FOREIGN KEY ('order_id_item') REFERENCES order_items('order_id_item'),
-	FOREIGN KEY ('cusotmer_id) REFERENCES customers('customer_id)
+	order_id int NOT NULL AUTO_INCREMENT,
+	customer_id int NOT NULL,
+	PRIMARY KEY (order_id),
+	FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
+
 );
+
 
 
