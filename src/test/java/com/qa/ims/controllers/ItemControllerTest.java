90b657cb582ed5981438633205575a05f66b465c
voytus <voytus@protonmail.com>
2022-05-10 13:34:47 +0100

    WTM added new branch feature

---
+++ src/test/java/com/qa/ims/controllers/ItemControllerTest.java
@@ -0,0 +1,88 @@
+package com.qa.ims.controllers;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+import com.qa.ims.controller.ItemController;
+import com.qa.ims.persistence.dao.ItemDAO;
+import com.qa.ims.persistence.domain.Item;
+import com.qa.ims.utils.Utils;
+
+@RunWith(MockitoJUnitRunner.class)
+public class ItemControllerTest {
+
+	@Mock
+	private Utils utils;
+
+	@Mock
+	private ItemDAO dao;
+
+	@InjectMocks
+	private ItemController controller;
+
+	@Test
+	public void testCreate() {
+		final String name = "pencil";
+		final Double itemPrice = 20D;
+		final Item created = new Item(name, itemPrice);
+
+		Mockito.when(utils.getString()).thenReturn(name);
+		Mockito.when(utils.getDouble()).thenReturn(itemPrice);
+
+		Mockito.when(dao.create(created)).thenReturn(created);
+
+		assertEquals(created, controller.create());
+
+		Mockito.verify(utils, Mockito.times(2)).getString();
+		Mockito.verify(dao, Mockito.times(1)).create(created);
+	}
+
+	@Test
+	public void testReadAll() {
+		List<Item> items = new ArrayList<>();
+		items.add(new Item(2D, "pencil", 20D));
+ 
+		Mockito.when(dao.readAll()).thenReturn(items);
+
+		assertEquals(items, controller.readAll());
+
+		Mockito.verify(dao, Mockito.times(1)).readAll();
+	}
+
+	@Test
+	public void testUpdate() {
+		Item updated = new Item(1L, "chris", "perrins");
+
+		Mockito.when(this.utils.getLong()).thenReturn(1L);
+		Mockito.when(this.utils.getString()).thenReturn(updated.getFirstName(), updated.getSurname());
+		Mockito.when(this.dao.update(updated)).thenReturn(updated);
+
+		assertEquals(updated, this.controller.update());
+
+		Mockito.verify(this.utils, Mockito.times(1)).getLong();
+		Mockito.verify(this.utils, Mockito.times(2)).getString();
+		Mockito.verify(this.dao, Mockito.times(1)).update(updated);
+	}
+
+	@Test
+	public void testDelete() {
+		final long ID = 1L;
+
+		Mockito.when(utils.getLong()).thenReturn(ID);
+		Mockito.when(dao.delete(ID)).thenReturn(1);
+
+		assertEquals(1L, this.controller.delete());
+
+		Mockito.verify(utils, Mockito.times(1)).getLong();
+		Mockito.verify(dao, Mockito.times(1)).delete(ID);
+	}
+
+}
